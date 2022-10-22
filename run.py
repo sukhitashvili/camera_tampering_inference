e1d03b1410f3934bf9b6656c691ad4eab3039fc5
sukhitashvili1 <soso.sukhitashvili@lean-ai-tech.com>
2022-10-22 19:03:04 +0400

    first commit

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
-import hydra
-
+from hydra import initialize, compose
 from omegaconf import DictConfig
 
+from core.inference_pipeline import InferencePipeline
 
-@hydra.main(config_name="config.yaml")
-def test_inference(config: DictConfig):
-    pass
+
+def get_config() -> DictConfig:
+    with initialize(config_path="."):
+        # config is relative to a module
+        config = compose(config_name="config")
+    return config
+
+
+def init():
+    config = get_config()
+    inference_object = InferencePipeline(config=config)
+    return inference_object
+
+
+if __name__ == "__main__":
+    inference_object = init()
+    inference_object.run()
+    print(inference_object)
